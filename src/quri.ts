07dd683292e4994dc288c66739f50836379260ed
Jessica Mulein <jessica@brightchain.org>
2022-06-16 14:05:45 -0700

    start setting up angular

--- src/quri.ts
+++ src/quri.ts
@@ -1,9 +1,9 @@
 //FileName : quri.ts
 /// <reference types="node" />
-"use strict";
-import { firebaseConfig } from "./firebaseConfig";
-import { initializeApp, FirebaseApp } from "firebase/app";
-import { IQuriApp } from "./interfaces";
+'use strict';
+import { firebaseConfig } from './firebaseConfig';
+import { initializeApp, FirebaseApp } from 'firebase/app';
+import { IQuriApp } from './interfaces';
 declare global {
   interface Window {
     quri: IQuriApp;
@@ -13,16 +13,27 @@ declare global {
   }
 }
 const _quri: IQuriApp = {
-  firebaseApp: window.firebase !== undefined &&
-  window.firebase.apps !== undefined &&
-  window.firebase.apps.length > 0 ? window.firebase.apps[0] : null,
+  firebaseApp:
+    window.firebase !== undefined &&
+    window.firebase.apps !== undefined &&
+    window.firebase.apps.length > 0 &&
+    window.firebase.apps[0] !== undefined &&
+    window.firebase.apps[0] !== null
+      ? window.firebase.apps[0]
+      : null,
   firebaseHosting:
     window.firebase !== undefined &&
     window.firebase.apps !== undefined &&
-    window.firebase.apps.length > 0,
+    window.firebase.apps.length > 0 &&
+    window.firebase.apps[0] !== undefined &&
+    window.firebase.apps[0] !== null,
   config: firebaseConfig,
   onLoad: function () {
-    const loadEl = document.querySelector("#load");
+    const loadEl = document.querySelector('#load');
+    if (loadEl === null) {
+      console.error('unable to find #load');
+      return;
+    }
     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
     // // The Firebase SDK is initialized and available here!
     //
@@ -37,36 +48,36 @@ const _quri: IQuriApp = {
     // firebase.performance(); // call to activate
     //
     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-    const allFeatures = [
-      "auth",
-      "database",
-      "firestore",
-      "functions",
-      "messaging",
-      "storage",
-      "analytics",
-      "remoteConfig",
-      "performance",
+    const allFeatures: Array<string> = [
+      'auth',
+      'database',
+      'firestore',
+      'functions',
+      'messaging',
+      'storage',
+      'analytics',
+      'remoteConfig',
+      'performance',
     ];
     try {
-      if (!_quri.firebaseHosting) {
+      if (_quri.firebaseApp === null || _quri.firebaseApp === undefined) {
         _quri.firebaseApp = initializeApp(firebaseConfig);
       }
-      const features = [];
+      const features: Array<string> = [];
       for (let x = 0; x < allFeatures.length; x++) {
         const feature = allFeatures[x];
-        if (typeof _quri.firebaseApp[feature] === "function") {
+        if (typeof _quri.firebaseApp[feature] === 'function') {
           features.push(feature);
         }
       }
-      loadEl.textContent = `Firebase SDK loaded with ${features.join(", ")}`;
+      loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
     } catch (e) {
       console.error(e);
-      loadEl.textContent = "Error loading the Firebase SDK, check the console.";
+      loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
     }
   },
 };
 window.quri = window.quri || _quri;
 
-document.addEventListener("DOMContentLoaded", window.quri.onLoad);
+document.addEventListener('DOMContentLoaded', window.quri.onLoad);
 export default { quri: _quri };
