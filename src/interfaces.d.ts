c1dc6f77f7de1670e26780168223c2dc9867f0a4
Jessica Mulein <jessica@brightchain.org>
2022-06-16 14:05:45 -0700

    set up angular

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -1,12 +1,13 @@
 /// <reference types="node" />
-import { FirebaseApp } from "firebase/app";
-import { Auth, GoogleAuthProvider, User, UserInfo } from "firebase/auth";
-import { Database } from "firebase/database";
-import { FieldValue, Firestore } from "firebase/firestore";
-import { auth as firebaseUiAuth } from "firebaseui";
+'use strict';
+import { FirebaseApp } from 'firebase/app';
+import { Auth, GoogleAuthProvider, User, UserInfo } from 'firebase/auth';
+import { Database } from 'firebase/database';
+import { FieldValue, Firestore } from 'firebase/firestore';
+import { auth as firebaseUiAuth } from 'firebaseui';
 interface IQuriApp {
   firebaseHosting: boolean;
-  firebaseApp?: FirebaseApp;
+  firebaseApp: FirebaseApp | null;
   config: firebaseConfig;
   onLoad: () => void;
 }
--- src/quri.ts
+++ src/quri.ts
@@ -1,9 +1,15 @@
 //FileName : quri.ts
 /// <reference types="node" />
-"use strict";
-import { firebaseConfig } from "./firebaseConfig";
-import { initializeApp, FirebaseApp } from "firebase/app";
-import { IQuriApp } from "./interfaces";
+'use strict';
+import { firebaseConfig } from './firebaseConfig';
+import { initializeApp, FirebaseApp } from 'firebase/app';
+import { IQuriApp } from './interfaces';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
 declare global {
   interface Window {
     quri: IQuriApp;
@@ -12,17 +18,29 @@ declare global {
     };
   }
 }
+
 const _quri: IQuriApp = {
-  firebaseApp: window.firebase !== undefined &&
-  window.firebase.apps !== undefined &&
-  window.firebase.apps.length > 0 ? window.firebase.apps[0] : null,
+  firebaseApp:
+    window.firebase !== undefined &&
+    window.firebase.apps !== undefined &&
+    window.firebase.apps.length > 0 &&
+    window.firebase.apps[0] !== undefined &&
+    window.firebase.apps[0] !== null
+      ? window.firebase.apps[0]
+      : null,
   firebaseHosting:
     window.firebase !== undefined &&
     window.firebase.apps !== undefined &&
-    window.firebase.apps.length > 0,
+    window.firebase.apps.length > 0 &&
+    window.firebase.apps[0] !== undefined &&
+    window.firebase.apps[0] !== null,
   config: firebaseConfig,
   onLoad: function () {
-    const loadEl = document.querySelector("#load");
+    const loadEl = document.querySelector('#load');
+    if (loadEl === null) {
+      console.error('unable to find #load');
+      return;
+    }
     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
     // // The Firebase SDK is initialized and available here!
     //
@@ -37,36 +55,45 @@ const _quri: IQuriApp = {
     // firebase.performance(); // call to activate
     //
     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-    const allFeatures = [
-      "auth",
-      "database",
-      "firestore",
-      "functions",
-      "messaging",
-      "storage",
-      "analytics",
-      "remoteConfig",
-      "performance",
+    const allFeatures: Array<string> = [
+      'auth',
+      'database',
+      'firestore',
+      'functions',
+      'messaging',
+      'storage',
+      'analytics',
+      'remoteConfig',
+      'performance',
     ];
     try {
-      if (!_quri.firebaseHosting) {
+      if (_quri.firebaseApp === null || _quri.firebaseApp === undefined) {
         _quri.firebaseApp = initializeApp(firebaseConfig);
       }
-      const features = [];
+      const features: Array<string> = [];
       for (let x = 0; x < allFeatures.length; x++) {
         const feature = allFeatures[x];
-        if (typeof _quri.firebaseApp[feature] === "function") {
+        if (typeof _quri.firebaseApp[feature] === 'function') {
           features.push(feature);
         }
       }
-      loadEl.textContent = `Firebase SDK loaded with ${features.join(", ")}`;
+      loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
     } catch (e) {
       console.error(e);
-      loadEl.textContent = "Error loading the Firebase SDK, check the console.";
+      loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
     }
   },
 };
 window.quri = window.quri || _quri;
 
-document.addEventListener("DOMContentLoaded", window.quri.onLoad);
+document.addEventListener('DOMContentLoaded', window.quri.onLoad);
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
 export default { quri: _quri };
